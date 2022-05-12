a77d5784934734aef49934006c3c5410dce09ab4
Megallix <megallix@gmail.com>
2022-05-12 04:43:11 -0700

    Layered Noise for Planetary Terrain
    #1
    Uses libnoise-dotnet for specialized coherent noise.

--- Assets/Planet Generation/ShapeSettings.cs
+++ Assets/Planet Generation/ShapeSettings.cs
@@ -7,4 +7,13 @@ public class ShapeSettings : ScriptableObject
 {
 
     public float planetRadius = 1;
+    public NoiseLayer[] noiseLayers;
+
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+        public bool useFirstLayerAsMask;
+        public NoiseSettings noiseSettings;
+    }
 }
