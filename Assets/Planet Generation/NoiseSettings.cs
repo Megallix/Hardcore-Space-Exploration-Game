7dc477cccfdd1d24f1fb30ff2cd2277e0bb0e4c1
Megallix <megallix@gmail.com>
2022-05-12 06:18:47 -0700

    Multiple Noise Filters
    #1
    
    Original version of the ConditionalHideAttribute script created by Brecht Lecluyse (www.brechtos.com)
    Modified by: Sebastian Lague

--- Assets/Planet Generation/NoiseSettings.cs
+++ Assets/Planet Generation/NoiseSettings.cs
@@ -6,12 +6,33 @@ using UnityEngine;
 public class NoiseSettings
 {
 
-    public float strength = 1;
-    [Range(1, 8)]
-    public int numLayers = 1;
-    public float baseRoughness = 1;
-    public float roughness = 2;
-    public float persistence = .5f;
-    public Vector3 center;
-    public float minValue;
+    public enum FilterType { Simple, Ridgid };
+    public FilterType filterType;
+
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
+    public RidgidNoiseSettings ridgidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        public float strength = 1;
+        [Range(1, 8)]
+        public int numLayers = 1;
+        public float baseRoughness = 1;
+        public float roughness = 2;
+        public float persistence = .5f;
+        public Vector3 center;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RidgidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = .8f;
+    }
+
+
+
 }
--- Assets/Planet Generation/Planet.cs
+++ Assets/Planet Generation/Planet.cs
@@ -8,6 +8,8 @@ public class Planet : MonoBehaviour
     [Range(2, 256)]
     public int resolution = 10;
     public bool autoUpdate = true;
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Front, Back };
+    public FaceRenderMask faceRenderMask;
 
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
@@ -48,6 +50,8 @@ public class Planet : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -78,9 +82,12 @@ public class Planet : MonoBehaviour
 
     void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
